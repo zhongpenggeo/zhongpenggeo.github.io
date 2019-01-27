b467a77efca1fd61716cca7d976ad05a97ef960f
zhongpenggeo <zhongpenggeo@gmail.com>
2019-01-27 21:17:08 +0800

    add categories in cayman

--- _posts/Communication/rename_all.sh
+++ _posts/Communication/rename_all.sh
@@ -13,19 +13,19 @@
  
 ALL=`ls *.md`
 # assume the rename time begin in a month ago;
-TM=`date -d "1 month ago" +%s`
+TM=0
+TM_B=2017-06-05
 for NA in $ALL
 do
 	# time past one day each loop, 86400 seconds eual 1 day
 	TM=`echo "$TM+86400"|bc`
 	# time format as 2008-1-2
-	TM_NA=`date -d "1970-01-01 $TM seconds" +"%Y-%m-%d"`
+	TM_NA=`date -d "$TN_B $TM seconds" +"%Y-%m-%d"`
     f1=`echo ${NA%.*}`
     f=`echo ${f1##*/}`
 	# match original filename and cut number in filename;
-	ff=`echo "$f"|sed -e "s/[0-9]-[0-9]*-//g"`
-	ff2=`echo "$ff"|sed -e "s/[0-9]-//g"`
+	ff=`echo "$f"|sed -e "s/-/_/g"`
 	# rename
-    NEWNA=$TM_NA-$ff2.md
+    NEWNA=$TM_NA-$ff.md
     mv $NA $NEWNA
 done
