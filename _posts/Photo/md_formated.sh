be800997129e5d7fcc46e71f3bc82610a01a9a80
zhongpenggeo <zhongpenggeo@gmail.com>
2019-01-29 19:50:14 +0800

    add design in cayman

--- _posts/Photo/md_formated.sh
+++ _posts/Photo/md_formated.sh
@@ -11,13 +11,13 @@
  
 ALL=`ls *.md`
 TM=0
-TM_BEGIN=2018-10-01
+TM_B=2018-10-01
 # get folder name
 DIR=`echo "$PWD"|gawk -F'/' '{print $NF}'`
 for NA in $ALL
 do
 	TM=`echo "$TM+86400"|bc`
-    TM_FM=`date -d "$TM_BEGIN $TM seconds" +"%Y-%m-%d %H:%M:%S"`
+    TM_FM=`date -d "$TM_B $TM seconds" +"%Y-%m-%d %H:%M:%S"`
 	F_LINE=`sed  '/^$/d' $NA|gawk 'NR==1 {print $1}'`
 	if [ $F_LINE != '---' ]; then
     	TMP=`echo ${NA%.*}`
@@ -29,5 +29,19 @@ do
     	sed -i "1i\title: $FI" $NA
     	sed -i '1i\---' $NA
 	fi
+	f1=`echo ${NA%.*}`
+    f=`echo ${f1##*/}`
+    Yr=`echo "$f"|cut -c 1-3`
+    if [[ "$Yr" =~ "201" ]]; then
+		# a=3 is onthing, jsut do a action.
+        a=3
+    else
+    	# time format as 2008-1-2
+    	TM_NA=`date -d "$TM_B $TM seconds" +"%Y-%m-%d"`
+    	# match original filename and cut number in filename;
+    	ff=`echo "$f"|sed -e "s/-/_/g"`
+    	# rename
+    	NEWNA=$TM_NA-$ff.md
+    	mv $NA $NEWNA
+    fi
 done
-
